e994766cff65443d433497bf64ae6aaa7b3c7c1f
steve <dz9c92@gmail.com>
2023-02-23 19:41:08 -0500

    2_23_6

--- w6-paycalc/scripts/script.js
+++ w6-paycalc/scripts/script.js
@@ -1,17 +1,14 @@
-const queryString = window.location.search;
-console.log(queryString);
+// gather user input
+let hourlyPay = Number(prompt("What is your hourly pay rate?"));
+let hoursWorked = Number(prompt("How many hours did you work in a two-week period?"));
 
-const urlParams = new URLSearchParams(queryString);
+// calculate gross pay, tax, and net pay
+let grossPay = hourlyPay * hoursWorked;
+let tax = grossPay * 0.2;
+let netPay = grossPay - tax;
 
-entries = urlParams.entries();
-
-var body = document.querySelector("body")
-for(const entry of entries) {
-    var p = document.createElement('p') 
-    var text = document.createTextNode(`${entry[0]}: ${entry[1]}`)
-    
-    body.appendChild(p)
-    p.appendChild(text)
-    
-    console.log(`${entry[0]}: ${entry[1]}`);
-}
+// format and display results using the toFixed method
+alert("Gross Pay: $" + grossPay.toFixed(2) + "\nUncle Sam's Share: $" + tax.toFixed(2) + "\nNet Pay: $" + netPay.toFixed(2));
+console.log("Gross Pay: $" + grossPay.toFixed(2));
+console.log("Uncle Sam's Share: $" + tax.toFixed(2));
+console.log("Net Pay: $" + netPay.toFixed(2));
